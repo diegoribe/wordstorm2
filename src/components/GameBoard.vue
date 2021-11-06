53685014997c0cdfc2804cd2fa9adac1c0bc15f9
diegoribe <diego@engineering.com>
2021-11-06 13:18:05 -0400

    Solve timing issue

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -13,8 +13,13 @@
             </div> -->
         </div>
         <div class="canvas" id="canvas">
-          <h4 :class="word" v-for="(word,index) in words"  :key="index">{{word}}</h4>
+            <transition v-for="(word,index) in words"  :key="index" name="rain" @after-enter="afterEnter">
+              <h4 v-if="show[index]" :class="word">{{word}}</h4>
+            </transition>
         </div>
+        <button @click="show = !show">
+          Toggle
+        </button>
         <div class="inputContainer">
           <form @submit.prevent="handleInput(this.wordInput)">
           <input v-model="wordInput" type="text" required class="inputText" placeholder="Enter Text" autocomplete="off">
@@ -25,23 +30,60 @@
 </template>
 
 <script>
+import randomWords from './randomWords'
+
 export default {
   name: 'GameBoard',
   data: function () {
     return {
-      words : ['paper'],
+      words : [],
       wordInput: '',
       score: 0,
-      lifes: 3
+      lifes: 3,
+      show: []
     }
+  },
+  mounted () {
+    window.setInterval(() => {
+      this.addWord()
+      this.addListener()
+    }, 1000)
   },
   methods: {
     handleInput: function(word) {
-      console.log(word)
+      // console.log(word)
       if (this.words.includes(word)) {
         this.words = this.words.filter((value)=>value !== word)
       }
-    }
+      this.wordInput = ''
+    },
+    addWord: function () {
+      const wordIndex = this.getRandomNumber()
+      // console.log(wordIndex)
+      this.words.push(randomWords[wordIndex])
+      const that = this
+      setTimeout(function () {
+        that.show.push(true)
+      }, 500)
+    },
+    getRandomNumber: function () {
+      return Math.floor(Math.random() * randomWords.length)
+    },
+    addListener: function () {
+      // const wordsCreated = document.querySelectorAll('h4')
+      // wordsCreated.addEventListener('animationend', () => {
+      //   console.log('Animation ended');
+      // })
+    },
+    enterAnimation: function () {
+      console.log('enter')
+    },
+    leaveAnimation: function () {
+      console.log('low')
+    },
+    afterEnter(el) {
+      console.log(el)
+    },
   }
 }
 </script>
@@ -69,26 +111,44 @@ export default {
 }
 h4 {
   color: white;
-  animation: rain 7s;
-  animation-fill-mode: forwards;
-  position: absolute;
-  animation-timing-function: linear;
   transform: rotate(40deg);
-  opacity: 1;
-  transition: opacity 0.2s ease-out;
   font-size: 2rem;
+  opacity: 1;
+  // transition: opacity 0.2s ease-out;
+  position: absolute;
 }
+
+.rain-enter-active, .rain-leave-active{
+  top: 0px;
+  animation: rain 7s;
+}
+
+// .rain-enter {
+//   animation: rain 7s;
+// }
+
 
 @keyframes rain {
   0% {
-    background: auto;
     top: 0px;
   }
   100% {
-    background: auto;
     top: 80%;
-    color: red;
+    // color: red;
   }
+}
+
+@keyframes rotate {
+    0% { opacity: 0; transform: scale(0) rotate(-180deg); }
+    100% { opacity: 1; transform: scale(1) rotate(0deg); }
+}
+
+.rotate-enter-active {
+    animation: rotate 0.2s;
+}
+
+.rotate-leave-active {
+    animation: rotate 0.2s reverse;
 }
 
 .inputContainer {
@@ -120,5 +180,9 @@ input[type="text"] {
   color: black;
   border: none;
   border-radius: 5px;
+}
+
+.hide {
+  opacity: 0;
 }
 </style>
